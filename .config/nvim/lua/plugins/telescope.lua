ccd48f1eb2e7cd80fff9d7e73444d8eaf1a6e6a6
serr <sc7227484@gmail.com>
2025-09-22 13:47:22 +0300

    quick update

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -46,6 +46,6 @@ return {
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope find file" })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope find content (grep)" })
         vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "Telescope find keymap" })
-        vim.keymap.set('n', '<leader>ft', ":TodoTelescope<CR>", { desc = "Telescope find todo comments" })
+        vim.keymap.set('n', '<leader>ft', ":TodoTelescope<CR>", { desc = "Telescope find todo comment" })
     end
 }

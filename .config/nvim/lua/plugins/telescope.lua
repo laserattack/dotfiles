5b941c5a09596ec0d480777e1f546da6936f380e
serr <sc7227484@gmail.com>
2025-09-24 12:00:44 +0300

    fix grep todo comments

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,17 @@
+local vimgrep_arguments = {
+    "rg",
+    "--hidden",
+    "--no-ignore",
+    "--color=never",
+    "--no-heading",
+    "--with-filename",
+    "--line-number",
+    "--column",
+    "--smart-case",
+    -- "--glob", "!**/deps/**",
+    "--glob", "!**/.git/**"
+}
+
 return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
@@ -26,27 +40,20 @@ return {
                     },
                 },
                 live_grep = {
-                    -- Аргументы по умолчанию + игнор директории с зависимостями
-                    vimgrep_arguments = {
-                        "rg",
-                        "--hidden",
-                        "--no-ignore",
-                        "--color=never",
-                        "--no-heading",
-                        "--with-filename",
-                        "--line-number",
-                        "--column",
-                        "--smart-case",
-                        -- "--glob", "!**/deps/**",
-                        "--glob", "!**/.git/**"
-                    }
+                    vimgrep_arguments = vimgrep_arguments
                 }
             },
         })
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope find file" })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope find content (grep)" })
         vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "Telescope find keymap" })
-        vim.keymap.set('n', '<leader>ft', ":TodoTelescope<CR>", { desc = "Telescope find todo comment" })
+        vim.keymap.set('n', '<leader>ft', function()
+            require('telescope.builtin').grep_string({
+                search = 'NOTE:|TODO:|FIX:|HACK:|WARN:|PERF:|TEST:',
+                use_regex = true,
+                vimgrep_arguments = vimgrep_arguments,
+            })
+        end, { desc = "Find TODO comments" })
         vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find, { desc = "Telescope find in current buffer" })
     end
 }

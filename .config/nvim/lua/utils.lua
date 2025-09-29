dd93654d1d0e4b7eaf8a45e9d699379d45353923
serr <sc7227484@gmail.com>
2025-09-29 16:36:09 +0300

    quick update

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,8 +1,10 @@
 local diagnostics_active = false
+
 local function update_statusline()
     local status = diagnostics_active and "D:ON" or "D:OFF"
     vim.opt.statusline = status .. " %f %h%w%m%r%=%l:%c %P"
 end
+
 local function toggle_diagnostics()
     diagnostics_active = not diagnostics_active
 

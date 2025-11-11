3d6e480503ffb4541018a65a43c3769cb6b5ab37
serr <sc7227484@gmail.com>
2025-11-11 13:02:00 +0300

    quick update

--- custom_i3status.sh
+++ custom_i3status.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
-# Обертка для i3status с добавлением текущей раскладки
 
 i3status | while :
 do
-        # Читаем строку из i3status
         read line
-
-        # Получаем текущую раскладку
         LAYOUT=$(xkb-switch)
-
-        # Выводим модифицированную строку
-        echo "layout: $LAYOUT | $line" || exit 1
+        echo "|$line|layout: $LAYOUT" || exit 1
 done
